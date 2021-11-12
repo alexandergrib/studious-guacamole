c3c131f4b2fa3bc4e4de29885b9cbe18cc1f9a0f
oljung <patrik_osterljung@hotmail.com>
2021-11-12 23:16:26 +0000

    feat: create update profile form

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,8 @@
 let anonymous_checkbox = document.getElementById("anonymous_checkbox");
 let userNameForm = document.getElementById("user_full_name");
 let userNickName = document.getElementById("user_nickname");
+let editProfile = document.getElementById('edit-profile');
+let editProfileForm = document.getElementById('edit-profile-form');
 
 
 anonymous_checkbox.addEventListener('change', function(){
@@ -13,3 +15,5 @@ anonymous_checkbox.addEventListener('change', function(){
             userNickName.classList.add('hide');
     }
 })
+
+editProfile.addEventListener('click', () => editProfileForm.classList.remove('hide'));
